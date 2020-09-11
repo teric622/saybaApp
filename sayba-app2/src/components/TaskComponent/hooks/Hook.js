3e46e97bb9f8810327b146a52c4412f590fe287d
teric622 <tlasecaeric@gmail.com>
2020-09-10 21:23:15 -0400

    havent broken nada

--- sayba-app2/src/components/TaskComponent/hooks/Hook.js
+++ sayba-app2/src/components/TaskComponent/hooks/Hook.js
@@ -6,6 +6,8 @@ import moment from 'moment';
 
 export const useTasks = selectedProject => {
     const [task, setTasks] = useState([]);
+    const [archivedTasks, setArchivedTasks] = useState([]);
+
 
 useEffect(() => {
 
@@ -21,11 +23,37 @@ unsubscribe = selectedProject &&  !collatedTasksExist(selectedProject)?
 ? (unsubscribe = unsubscribe.where
     ('date', '==',moment().format('DD/MM/YYY')
     ))
+
 :selectedProject === 'INBOX' || selectedProject === 0
 ? (unsubscribe = unsubscribe.where('date', '==', ''))
 : unsubscribe;
 
+
+unsubscribe = unsubscribe.onSnapshot (snapshot => {
+    const newTasks = snapshot.docs.map (task => ({
+    id: task.id,
+    ...task.data(),
+    }));
+
+   setTasks (
+       selectedProject === 'NEXT_7'
+       ? newTasks.filter(
+task => 
+moment (task.date, 'DD-MM-YYYY').diff(moment(), 'days') <= 7 &&
+task.archived !==true
+       )
+        : newTasks.filter(task => task.archived !==true)
+   );
+
+
+
+   setArchivedTasks(newTasks.filter(task => task.archived !== false))
+
+});
+
 }, [selectedProject]);
 
 
-}
+};
+
+// 59:09  = vidk 3909 895u 8302-jn hb uuu9n hbu9j
