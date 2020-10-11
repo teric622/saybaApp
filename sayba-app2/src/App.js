8694618fec0fedb7bddfa7c083b76f42a8ee680a
teric622 <tlasecaeric@gmail.com>
2020-10-11 00:22:04 -0400

    transition was not a scuccess

--- sayba-app2/src/App.js
+++ sayba-app2/src/App.js
@@ -2,6 +2,8 @@ import React from 'react';
 import {BrowserRouter as Router, Switch, Route, Link} from "react-router-dom";
 import One from '../src/components/OneComponent/One'
 import Messaging from './components/MessagingComponent/Messaging'
+import {CSSTransition,TransitionGroup,} from 'react-transition-group';
+import uuid from 'uuid';
 
 
 // import Manager from '../src/components/ManagerComponent/Manager'
@@ -10,7 +12,7 @@ import TwoDoMotherBoard from '../src/components/TwoDoComponent/TwoDoMotherBoard'
 import Home from './components/HomeComponent/Home';
 // import Msging from './components/MessagingComponent/Msging'
 import Relax from './components/RelaxComponent/Relax';
-
+import './App.css';
 
 
 function App() {
@@ -19,6 +21,15 @@ function App() {
   return (
     <Router>
     <div className="app">
+
+<Route render={(location)=> (
+ <TransitionGroup>
+ <CSSTransition
+ key={location.key}
+           timeout={300}
+           classNames="item">
+
+
       <Switch>
           <Route exact path='/' component={One} />
           <Route path='/rooms/:roomId' component={Messaging} />
@@ -27,6 +38,9 @@ function App() {
           <Route exact path='/:roomId/video' component={TwoDoMotherBoard} />
           <Route exact path='/:roomId/balance' component={Relax} />
         </Switch>
+        </CSSTransition>
+        </TransitionGroup>
+)} />
       </div>
     </Router>
          
