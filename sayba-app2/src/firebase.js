f2a27fa91798bc90f41e0e5dd21b65b926efd0b6
teric622 <tlasecaeric@gmail.com>
2020-10-23 18:24:37 -0400

    tdb

--- sayba-app2/src/firebase.js
+++ sayba-app2/src/firebase.js
@@ -13,16 +13,32 @@ import firebase from "firebase";
 //     measurementId: "G-BVEFNVPR87"
 //   };
 
+// const firebaseConfig = {
+//   apiKey: "AIzaSyAe-rLYQncjtqpAWoyjNdaKUubJRaBvk_g",
+//   authDomain: "nezapp-76c1b.firebaseapp.com",
+//   databaseURL: "https://nezapp-76c1b.firebaseio.com",
+//   projectId: "nezapp-76c1b",
+//   storageBucket: "nezapp-76c1b.appspot.com",
+//   messagingSenderId: "831702249365",
+//   appId: "1:831702249365:web:7ebf21b03fe130d7c9909c",
+//   measurementId: "G-XP8WYQGJXV"
+// };
+
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAe-rLYQncjtqpAWoyjNdaKUubJRaBvk_g",
-  authDomain: "nezapp-76c1b.firebaseapp.com",
-  databaseURL: "https://nezapp-76c1b.firebaseio.com",
-  projectId: "nezapp-76c1b",
-  storageBucket: "nezapp-76c1b.appspot.com",
-  messagingSenderId: "831702249365",
-  appId: "1:831702249365:web:7ebf21b03fe130d7c9909c",
-  measurementId: "G-XP8WYQGJXV"
+  apiKey: "AIzaSyDm-tFVc0TPB7ElSDSZ8vqGfPIPoCSdf98",
+  authDomain: "nez-app-et.firebaseapp.com",
+  databaseURL: "https://nez-app-et.firebaseio.com",
+  projectId: "nez-app-et",
+  storageBucket: "nez-app-et.appspot.com",
+  messagingSenderId: "356280542908",
+  appId: "1:356280542908:web:2fc9c0e71d9a738acc92b9",
+  measurementId: "G-JTX9GSX65B"
 };
+
+
+
 
   const firebaseApp = firebase.initializeApp(firebaseConfig);
   const db = firebaseApp.firestore();
